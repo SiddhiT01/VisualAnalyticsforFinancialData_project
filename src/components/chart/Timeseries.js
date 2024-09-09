998239f658beddadf40cb1c6b38578999b317af3
Siddhi <siddhitanawade01@gmail.com>
2024-09-09 23:29:22 +0100

    Your commit message describing the changes

--- src/components/chart/Timeseries.js
+++ src/components/chart/Timeseries.js
@@ -1,37 +1,24 @@
 import * as React from 'react';
 import BaseChart from "./BaseChart";
 
-const Timeseries = ({ data, options, onHoverPoint }) => {
+const Timeseries = ({ data, options }) => {
   const defaultOptions = {
     chart: {
       type: 'line',
     },
     xAxis: {
       type: 'datetime',
-    },
-    plotOptions: {
-      series: {
-        point: {
-          events: {
-            mouseOver: function () {
-              if (onHoverPoint) {
-                onHoverPoint(this); // Pass the hovered point to the parent
-              }
-            }
-          }
-        }
-      }
     }
-  };
+  }
 
   const finalOptions = {
     ...defaultOptions,
     ...options,
-  };
+  }
 
   return (
     <BaseChart options={finalOptions} data={data} />
-  );
-};
+  )
+}
 
 export default Timeseries;
